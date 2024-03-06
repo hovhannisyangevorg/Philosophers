1bc1b7d1f4ce90d43d816e095f4e9f2493cd7a69
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-03-06 15:52:05 +0400

    create ft_get_time.c functoinal.

--- Includes/philo_struct.h
+++ Includes/philo_struct.h
@@ -6,7 +6,7 @@
 /*   By: gevorg <gevorg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:19:38 by gevorg            #+#    #+#             */
-/*   Updated: 2024/03/01 18:11:13 by gevorg           ###   ########.fr       */
+/*   Updated: 2024/03/06 15:51:22 by gevorg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,14 @@
 #define MAX 6
 #define MIN 5
 
-// typedef struct 	s_philo_node	t_philo_node;
-// typedef struct	s_input	t_input;
-// typedef struct	s_vector t_vector;
+typedef struct 	s_global	t_global;
+typedef struct	s_input	t_input;
 
 typedef void*	(*t_thread_handler)(void*);
 
 typedef struct s_info
 {
-	unsigned long	last_eat;
+	size_t			last_eat; //-
 	int				*is_dead;
 	int				id;
 	int				date_of_eat;
@@ -45,11 +44,12 @@ typedef struct s_info
 	pthread_mutex_t	*mutex_write;
 	pthread_mutex_t	*left;
 	pthread_mutex_t	*right;
+
 }		t_info;
 
 typedef struct s_global
 {
-	unsigned long	time_to_die;
+	size_t			time_to_die; //-
 	int				count_philo;
 	int				max_eat;
 	int				is_dead;
@@ -58,34 +58,5 @@ typedef struct s_global
 	pthread_mutex_t	mutex_die;
 	pthread_mutex_t	*mutex;
 }		t_global;
-
-// struct s_philo_node
-// {
-// 	pthread_t			philo;
-// 	size_t				philo_id;
-// 	size_t				last_eat;
-// 	size_t				time_of_eat; // need
-// 	size_t				count_eat;	
-// 	size_t				time_of_sleep; // need
-// 	size_t				*is_dead;
-
-
-	
-// 	pthread_mutex_t		last_eat_mutex;
-// 	pthread_mutex_t		each_eat;
-// 	pthread_mutex_t		*mutex_die;
-// 	pthread_mutex_t		*mutex_write;	
-// 	pthread_mutex_t		*left;
-// 	pthread_mutex_t		*right;
-// };
-
-// struct s_input
-// {
-// 	size_t	number_of_philo;
-// 	size_t	time_to_die;
-// 	size_t	is_dead;
-// 	size_t	philosopher_must_eat;
-// 	int 	optional_must_eat;
-// };
 
 #endif
---
+++ Src/ft_get_time.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_time.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gevorg <gevorg@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 15:44:24 by gevorg            #+#    #+#             */
+/*   Updated: 2024/03/06 15:49:35 by gevorg           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+size_t	ft_get_time(void);
+void	ft_usleap(unsigned long t1);
+
+
+size_t	ft_get_time(void)
+{
+	static size_t				start;
+	static int					call_count;
+	struct timeval				t1;
+
+	if (call_count == 0)
+	{
+		gettimeofday(&t1, NULL);
+		start = (t1.tv_sec * 1000 + t1.tv_usec / 1000);
+		call_count++;
+	}
+	gettimeofday(&t1, NULL);
+	return ((t1.tv_sec * 1000) + (t1.tv_usec / 1000) - start);
+}
+
+void	ft_usleap(unsigned long t1)
+{
+	unsigned long	val;
+
+	val = ft_get_time();
+	while (ft_get_time() - val <= t1)
+		usleep(50);
+}
